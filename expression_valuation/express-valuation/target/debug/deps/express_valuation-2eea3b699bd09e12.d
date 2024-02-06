274fc664d74ae9eb79f8dc91bcf744e13ed5351f
alobogiron <andregiron.20221@poli.ufrj.br>
2024-02-06 10:42:10 -0300

    express valuation exer

---
+++ expression_valuation/express-valuation/target/debug/deps/express_valuation-2eea3b699bd09e12.d
@@ -0,0 +1,5 @@
+/home/alobogiron/code/begin_rust/ComprehensiveRustExercises/expression_valuation/express-valuation/target/debug/deps/express_valuation-2eea3b699bd09e12: src/main.rs
+
+/home/alobogiron/code/begin_rust/ComprehensiveRustExercises/expression_valuation/express-valuation/target/debug/deps/express_valuation-2eea3b699bd09e12.d: src/main.rs
+
+src/main.rs:
---
+++ expression_valuation/express-valuation/target/debug/deps/express_valuation-d66aaeb1c7489ef0.d
@@ -0,0 +1,5 @@
+/home/alobogiron/code/begin_rust/ComprehensiveRustExercises/expression_valuation/express-valuation/target/debug/deps/libexpress_valuation-d66aaeb1c7489ef0.rmeta: src/main.rs
+
+/home/alobogiron/code/begin_rust/ComprehensiveRustExercises/expression_valuation/express-valuation/target/debug/deps/express_valuation-d66aaeb1c7489ef0.d: src/main.rs
+
+src/main.rs:
